873fb351cb13ca5412ba1f000377af7a185b514e
Pauloice-byte <pauloice36@gmail.com>
2026-06-30 23:30:27 +0100

    Update script.js

--- Nova pasta/script.js
+++ Nova pasta/script.js
@@ -130,13 +130,46 @@ document.querySelectorAll(".course-card").forEach(card => {
 
 const themeToggle = document.getElementById("themeToggle");
 
+const sunIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+viewBox="0 0 24 24" fill="none"
+stroke="currentColor" stroke-width="2"
+stroke-linecap="round" stroke-linejoin="round">
+    <circle cx="12" cy="12" r="5"></circle>
+    <line x1="12" y1="1" x2="12" y2="3"></line>
+    <line x1="12" y1="21" x2="12" y2="23"></line>
+    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+    <line x1="1" y1="12" x2="3" y2="12"></line>
+    <line x1="21" y1="12" x2="23" y2="12"></line>
+    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+</svg>
+`;
+
+const moonIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+viewBox="0 0 24 24" fill="none"
+stroke="currentColor" stroke-width="2"
+stroke-linecap="round" stroke-linejoin="round">
+    <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+</svg>
+`;
+
+function updateThemeIcon() {
+    if (document.body.classList.contains("light-mode")) {
+        themeToggle.innerHTML = moonIcon;
+    } else {
+        themeToggle.innerHTML = sunIcon;
+    }
+}
+
 // Load saved theme
 if (localStorage.getItem("theme") === "light") {
     document.body.classList.add("light-mode");
-    themeToggle.textContent = "🌙";
-} else {
-    themeToggle.textContent = "☀️";
 }
+
+updateThemeIcon();
 
 // Toggle theme
 themeToggle.addEventListener("click", () => {
@@ -145,10 +178,29 @@ themeToggle.addEventListener("click", () => {
 
     if (document.body.classList.contains("light-mode")) {
         localStorage.setItem("theme", "light");
-        themeToggle.textContent = "🌙";
     } else {
         localStorage.setItem("theme", "dark");
-        themeToggle.textContent = "☀️";
     }
 
+    updateThemeIcon();
+
 });
+// =========================
+// BACKGROUND SLIDESHOW
+// =========================
+
+const slides = document.querySelectorAll(".bg-slide");
+
+let currentSlide = 0;
+
+slides[currentSlide].classList.add("active");
+
+setInterval(() => {
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+
+}, 3000);
