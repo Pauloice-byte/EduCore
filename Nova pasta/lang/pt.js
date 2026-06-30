0b8ced0477b3d49db4995e3dcfc565f69c1de2b3
Pauloice-byte <pauloice36@gmail.com>
2026-06-30 23:27:11 +0100

    Create pt.js

---
+++ Nova pasta/lang/pt.js
@@ -0,0 +1,14 @@
+const pt = {
+
+    pageTitle: "EduCore | Competências Práticas para o Mundo Digital",
+
+    heroTitle: "Aprenda Competências Práticas.",
+
+    heroSubtitle: "Construa o Seu Futuro.",
+
+    heroDescription: "...",
+
+    navCourses: "Cursos"
+
+    // ...
+};
