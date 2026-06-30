d3235cc5448986b47d32fde965c37ab4f6123398
Pauloice-byte <pauloice36@gmail.com>
2026-06-30 23:26:48 +0100

    Create fr.js

---
+++ Nova pasta/lang/fr.js
@@ -0,0 +1,14 @@
+const fr = {
+
+    pageTitle: "EduCore | Compétences Pratiques pour le Monde Numérique",
+
+    heroTitle: "Apprenez des Compétences Pratiques.",
+
+    heroSubtitle: "Construisez Votre Avenir.",
+
+    heroDescription: "...",
+
+    navCourses: "Cours"
+
+    // ...
+};
